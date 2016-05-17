8936cc4b067b13d12f1911481d42e91a1aeb56f4
Anoop Sarkar <anoop+github@cs.sfu.ca>
2016-05-16 23:52:35 -0700

    adhoc scanner updated

--- assets/demos/week2/adhocScanner.c
+++ assets/demos/week2/adhocScanner.c
@@ -11,7 +11,6 @@
 #define T_LT          '<'  
 #define T_GT          '>'  
 #define T_DIVIDE      '/'     
-#define T_CONCAT      '.'     
 // ...    
 
 #define T_WHILE       257  // reserved words   
@@ -38,10 +37,11 @@ struct token_t {
 int
 lookup_reserved (const char *kw)  
 {    
-  if (strcmp("WHILE", kw) == 0) return T_WHILE;
-  if (strcmp("IF", kw) == 0) return T_IF;
-  if (strcmp("RETURN", kw) == 0) return T_RETURN;
+  if (strcmp("while", kw) == 0) return T_WHILE;
+  if (strcmp("if", kw) == 0) return T_IF;
+  if (strcmp("return", kw) == 0) return T_RETURN;
   // ...
+  return -1;
 }    
 
 static int 
@@ -54,11 +54,14 @@ ScanOneToken (FILE *fp, struct token_t *token)
   switch(ch) {      
   case '/':            // could either begin comment or T_DIVIDE op     
     nextch = getc(fp);        
-    if (nextch == '/' || nextch == '*')          
+    if (nextch == '/' || nextch == '*') {
       ; // here you would skip over the comment     
-    else           
+    } else {
       ungetc(nextch, fp); // fall-through to single-char token case
-  case ';': case '(': case ')': case ',': case '=':    // ... and other single char tokens         
+      token->type = ch;
+	  return ch;
+	}
+  case ';': case '(': case ')': case ',': case '=': case '<': case '>':  // ... and other single char tokens         
     token->type = ch;              // ASCII value is used as token type        
     return ch;                     // ASCII value used as token type           
   case '\"':
@@ -87,12 +90,17 @@ ScanOneToken (FILE *fp, struct token_t *token)
   case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
   case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
   case 'v': case 'w': case 'x': case 'y': case 'z': 
-    token->type = T_IDENTIFIER;     
     token->val.stringValue[0] = ch;        
     for (i = 1; islower(ch = getc(fp)); i++)           
       token->val.stringValue[i] = ch; // gather lowercase     
     ungetc(ch, fp);      
     token->val.stringValue[i] = '\0';     
+	int lookup_type = lookup_reserved(token->val.stringValue);     
+	if (lookup_type != -1) {
+    	token->type = lookup_type;
+		return lookup_type;           
+	}
+    token->type = T_IDENTIFIER;     
     return T_IDENTIFIER;        
   case '0': case '1': case '2': case '3': case '4': 
   case '5': case '6': case '7': case '8': case '9':
@@ -119,25 +127,22 @@ main (int argc, char *argv[])
     // here is where you would process each token
     // printf("type:%d", token.type);
     switch(token.type) {
-    case T_WHILE: printf("%s","T_WHILE"); case T_IF: printf("%s","T_IF"); 
-    case T_RETURN: printf("%s","T_RETURN"); case T_IDENTIFIER: printf("%s","T_IDENTIFIER"); 
-      printf(" value:\'%s\'", token.val.stringValue);
-      break;
-    case T_STRING:
-      printf("T_STRING value:\"%s\"", token.val.stringValue);
-      break;
-    case T_INTEGER:
-      printf("T_INTEGER value:%d", token.val.intValue);
-      break;
-    case T_DOUBLE:
-      printf("T_DOUBLE value:%lf", token.val.doubleValue);
-      break;
-    default:
+    case T_WHILE: printf("%s","T_WHILE"); break; 
+	case T_IF: printf("%s","T_IF");  break;
+    case T_RETURN: printf("%s","T_RETURN"); break; 
+	case T_IDENTIFIER: printf("%s","T_IDENTIFIER"); printf(" value:\'%s\'", token.val.stringValue); break;
+    case T_STRING: printf("T_STRING value:\"%s\"", token.val.stringValue); break;
+    case T_INTEGER: printf("T_INTEGER value:%d", token.val.intValue); break;
+    case T_DOUBLE: printf("T_DOUBLE value:%lf", token.val.doubleValue); break;
+	default:
       if (token.type < 127) {
         switch(token.type) {
         case T_LPAREN: printf("T_LPAREN value:\'%c\'", token.type); break;
         case T_RPAREN: printf("T_RPAREN value:\'%c\'", token.type); break;
         case T_EQ: printf("T_EQ value:\'%c\'", token.type); break;
+        case T_LT: printf("T_LT value:\'%c\'", token.type); break;
+        case T_GT: printf("T_GT value:\'%c\'", token.type); break;
+        case T_DIVIDE: printf("T_DIVIDE value:\'%c\'", token.type); break;
         default: printf("T_CHAR value:\'%c\'", token.type);
         }
       } else {
