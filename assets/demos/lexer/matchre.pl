25f7048c9f0129581db0b20430f5ffe5f039f979
Anoop Sarkar <anoop+github@cs.sfu.ca>
2016-06-09 11:19:24 -0700

    moved some demo files

--- assets/demos/lexer/matchre.pl
+++
@@ -1,8 +0,0 @@
-while (<>) {
-    chomp($_);
-    if ( m/^((a|b)+(b|c)+)+d$/ ) {
-        print "yes\n";
-    } else {
-        print "no\n";
-    }
-}
--- assets/demos/lexer/matchre.py
+++
@@ -1,7 +0,0 @@
-import re
-import sys
-for line in sys.stdin:
-    if re.match('^((a|b)+(b|c)+)+d$', line):
-        print 'yes' 
-    else:
-        print 'no'
