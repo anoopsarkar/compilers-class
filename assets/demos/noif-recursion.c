ded38300b88e4abc46c66ab83ed9bd14ed1b6d18
Anoop Sarkar <anoop+github@cs.sfu.ca>
2017-05-19 14:27:57 -0700

    recursion without branching

---
+++ assets/demos/noif-recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+double (*farr[2])();
+
+double _fact(double n) {
+	if (n == 0) { return 1; }
+	else { return n * _fact(n-1); }
+}
+
+double bot(double n) {
+	return 1;
+}
+
+double fact(double n) {
+	return n*(*farr[n>1])(n-1);
+}
+
+int main() {
+	farr[0] = &bot;
+	farr[1] = &fact;
+
+	printf("%f\n", fact(9));
+	printf("%f\n", _fact(9));
+	return 0;
+}
