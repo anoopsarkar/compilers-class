5877e3101f954635bf65ba08ecb36265ee1cc795
Anoop Sarkar <anoop+github@cs.sfu.ca>
2020-08-31 00:02:41 -0700

    updated intro demos

--- assets/demos/intro/myquine.c
+++ assets/demos/intro/myquine.c
@@ -1,1 +1,2 @@
-main(){char *c="main(){char *c=%c%s%c;printf(c,34,c,34,10);}%c";printf(c,34,c,34,10);}
+#include <stdio.h>
+int main(){char *c="#include <stdio.h>%cint main(){char *c=%c%s%c;printf(c,10,34,c,34,10);}%c";printf(c,10,34,c,34,10);}
