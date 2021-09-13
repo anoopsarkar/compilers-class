fccff453d96c01cc665243d8c9b485d976acf296
Anoop Sarkar <193767+anoopsarkar@users.noreply.github.com>
2021-09-13 10:33:21 -0700

    added count as arg to send2

--- assets/demos/intro/send2.c
+++ assets/demos/intro/send2.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <sys/time.h>
-
-
-/* pick BUFLEN to be a suitably large number to show the speed
-   difference between send and send2 */
-const size_t BUFLEN = 400000000;
 
 void send2 (char *to, char *from, int count)
 {
@@ -30,23 +26,67 @@ void send (char *to, char *from, int count)
     *to++ = *from++;
 }
 
+bool check(char *to, char *from, int count)
+{
+  while (count-- > 0)
+    if (!(*to++ == *from++)) return false;
+  return true;
+}
+
 int main (int argc, char **argv)
 {
-  char *from, *to;
+  char *from, *to, *from2, *to2;
   int i;
   struct timeval before, after;
+  int count;
 
-  from = (char *) malloc(BUFLEN * sizeof(char));
-  to = (char *) malloc(BUFLEN * sizeof(char));
+  if (argc < 2) {
+    printf("provide count\n");
+    exit(1);
+  }
+  count = atoi(argv[1]);
+  printf("using count %d\n", count);
 
-  memset(from, 'a', (BUFLEN * sizeof(char)));
-  memset(to, 'b', (BUFLEN * sizeof(char)));
+  from = (char *) malloc(count * sizeof(char));
+  to = (char *) malloc(count * sizeof(char));
+  from2 = (char *) malloc(count * sizeof(char));
+  to2 = (char *) malloc(count * sizeof(char));
+
+  memset(from, 'a', (count * sizeof(char)));
+  memset(to, 'b', (count * sizeof(char)));
+  memset(from2, 'a', (count * sizeof(char)));
+  memset(to2, 'b', (count * sizeof(char)));
+
   printf("array init done\n");
 
   printf("calling send2\n");
   gettimeofday(&before, NULL);
-  send2(to, from, BUFLEN);
+  send2(to2, from2, count);
   gettimeofday(&after, NULL);
-  printf("secs=%d\n", (int)after.tv_sec - (int)before.tv_sec);
-  printf("microsecs=%d\n", abs((int)after.tv_usec - (int)before.tv_usec));
+  printf("send2 secs=%d\n", (int)after.tv_sec - (int)before.tv_sec);
+  printf("send2 microsecs=%d\n", abs((int)after.tv_usec - (int)before.tv_usec));
+
+  printf("calling send\n");
+  gettimeofday(&before, NULL);
+  send(to, from, count);
+  gettimeofday(&after, NULL);
+  printf("send secs=%d\n", (int)after.tv_sec - (int)before.tv_sec);
+  printf("send microsecs=%d\n", abs((int)after.tv_usec - (int)before.tv_usec));
+
+  if (check(to, from, count)) {
+    printf("check to, from passed\n");
+  } else {
+    printf("check to, from failed\n");
+  }
+  if (check(to, to2, count)) {
+    printf("check to, to2 passed\n");
+  } else {
+    printf("check to, to2 failed\n");
+  }
+  if (check(from, from2, count)) {
+    printf("check from, from2 passed\n");
+  } else {
+    printf("check from, from2 failed\n");
+  }
+
 }
